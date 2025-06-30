d9ee59c41210bee2d8dc4429d07579f1eebe73f0
Wagner Salbego <105342@urisantiago.br>
2025-06-29 21:11:40 -0300

    Avaliações do front

--- src/main/java/com/project/movieapi/controller/UserController.java
+++ src/main/java/com/project/movieapi/controller/UserController.java
@@ -3,7 +3,10 @@ package com.project.movieapi.controller;
 import com.project.movieapi.model.User;
 import com.project.movieapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -13,8 +16,29 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    // Cadastro de usuário
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    // Login de usuário
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginUser) {
+        System.out.println("Tentativa de login recebida:");
+        System.out.println("Email: " + loginUser.getEmail());
+        System.out.println("Senha: " + loginUser.getPassword());
+
+        Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
+
+        if (user.isPresent()) {
+            if (user.get().getPassword().equals(loginUser.getPassword())) {
+                return ResponseEntity.ok(user.get());
+            } else {
+                return ResponseEntity.status(401).body("Senha incorreta.");
+            }
+        } else {
+            return ResponseEntity.status(404).body("Usuário não encontrado.");
+        }
     }
 }
--- src/main/java/com/project/movieapi/repository/UserRepository.java
+++ src/main/java/com/project/movieapi/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.project.movieapi.repository;
 import com.project.movieapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
